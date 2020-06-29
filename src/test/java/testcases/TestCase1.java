3a482bfc72e86ef9634e54663f495805e2d8fff0
amazing010692 <hello_janielle1992@yahoo.com.ph>
2020-06-29 20:50:47 +0800

    4th commit - TestNG - Creating the First Test Case

--- src/test/java/testcases/TestCase1.java
+++ src/test/java/testcases/TestCase1.java
@@ -4,14 +4,14 @@ import org.testng.annotations.Test;
 
 public class TestCase1 {
 	
-	@Test
-	public void doLogin() {
-		System.out.println("Executing login test");
-	}
-	
-	@Test
+	@Test(priority = 1)
 	public void doUserReg() {
 		System.out.println("Executing User Registration test");
+	}
+	
+	@Test(priority = 2)
+	public void doLogin() {
+		System.out.println("Executing login test");
 	}
 
 }
