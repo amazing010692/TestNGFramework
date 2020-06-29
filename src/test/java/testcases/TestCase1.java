169a039bc568b5a0722081ee167c391bddd5d7c1
amazing010692 <hello_janielle1992@yahoo.com.ph>
2020-06-29 21:20:47 +0800

    7th commit - TestNG - Creating the First Test Case

--- src/test/java/testcases/TestCase1.java
+++ src/test/java/testcases/TestCase1.java
@@ -1,10 +1,22 @@
 package testcases;
 
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class TestCase1 {
+	
+	@BeforeTest
+	public void creatingDBConn() {
+		System.out.println("Creating DB connection");
+	}
+	
+	@AfterTest
+	public void closingDBConn() {
+		System.out.println("Closing DB connection");
+	}
 	
 	@BeforeMethod
 	public void launchBrowser() {
