1f7e2f6cf0f36a7d1a13c4d3e66d3bb01b2a2e4a
amazing010692 <hello_janielle1992@yahoo.com.ph>
2020-06-29 20:36:29 +0800

    2nd commit - TestNG - Creating the First Test Case

--- src/test/java/testcases/TestCase1.java
+++ src/test/java/testcases/TestCase1.java
@@ -1,9 +1,17 @@
 package testcases;
 
+import org.testng.annotations.Test;
+
 public class TestCase1 {
 	
+	@Test
 	public void doLogin() {
 		System.out.println("Executing login test");
+	}
+	
+	@Test
+	public void doUserReg() {
+		System.out.println("Executing User Registration test");
 	}
 
 }
