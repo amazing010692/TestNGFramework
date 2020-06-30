cbab997ff26b68f28fa15a481b057952f743f8a1
amazing010692 <hello_janielle1992@yahoo.com.ph>
2020-06-30 17:15:50 +0800

    1st commit - TestNG - Adding Assertions and Reporting Failures

---
+++ src/test/java/testcases/TestCase2.java
@@ -0,0 +1,19 @@
+package testcases;
+
+import org.testng.annotations.Test;
+
+public class TestCase2 {
+	
+	@Test
+	public void validateTitles() {
+		String expected_title = "Yahoo.com";	//excel
+		String actual_title = "Gmail.com";		//selenium
+		
+		if(expected_title.equals(actual_title)) {
+			System.out.println("Test Case Passed.");
+		} else {
+			System.out.println("Test Case Failed.");
+		}
+	}
+
+}
