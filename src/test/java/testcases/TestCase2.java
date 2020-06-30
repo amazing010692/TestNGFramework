9d62911edb86640d7a6d9ce3ec48b17b85e62dbc
amazing010692 <hello_janielle1992@yahoo.com.ph>
2020-06-30 18:21:02 +0800

    2nd commit - TestNG - Soft Assertions and Reporting Multiple failures

--- src/test/java/testcases/TestCase2.java
+++ src/test/java/testcases/TestCase2.java
@@ -7,6 +7,7 @@ public class TestCase2 {
 	
 	@Test
 	public void validateTitles() {
+		System.out.println("Beginning");
 		/*String expected_title = "Yahoo.com";	//excel
 		String actual_title = "Gmail.com";		//selenium
 		
@@ -24,6 +25,7 @@ public class TestCase2 {
 		
 		//Forcefully fail a test case.
 		Assert.fail("Failing the test as the condition is not met.");
+		System.out.println("Ending");
 	}
 
 }
