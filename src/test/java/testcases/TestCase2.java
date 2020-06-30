d5d66dc73d456aa40ce92fd988c9a0179d3af170
amazing010692 <hello_janielle1992@yahoo.com.ph>
2020-06-30 17:45:57 +0800

    7th commit - TestNG - Adding Assertions and Reporting Failures

--- src/test/java/testcases/TestCase2.java
+++ src/test/java/testcases/TestCase2.java
@@ -7,10 +7,10 @@ public class TestCase2 {
 	
 	@Test
 	public void validateTitles() {
-		String expected_title = "Yahoo.com";	//excel
+		/*String expected_title = "Yahoo.com";	//excel
 		String actual_title = "Gmail.com";		//selenium
 		
-		/*if(expected_title.equals(actual_title)) {
+		if(expected_title.equals(actual_title)) {
 			System.out.println("Test Case Passed.");
 		} else {
 			System.out.println("Test Case Failed.");
