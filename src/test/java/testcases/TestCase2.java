bbad2ce2226954ef74e35307abe3fe4bc5391cb0
amazing010692 <hello_janielle1992@yahoo.com.ph>
2020-06-30 19:02:50 +0800

    4th commit - TestNG - Soft Assertions and Reporting Multiple failures

--- src/test/java/testcases/TestCase2.java
+++ src/test/java/testcases/TestCase2.java
@@ -9,23 +9,30 @@ public class TestCase2 {
 	public void validateTitles() {
 		System.out.println("Beginning");
 		
-		/*String expected_title = "Yahoo.com";	//excel
+		String expected_title = "Yahoo.com";	//excel
 		String actual_title = "Gmail.com";		//selenium
 		
-		if(expected_title.equals(actual_title)) {
+		/*if(expected_title.equals(actual_title)) {
 			System.out.println("Test Case Passed.");
 		} else {
 			System.out.println("Test Case Failed.");
 		}*/
 		
-		/*//Compare an actual result vs expected result.
-		Assert.assertEquals(actual_title, expected_title);*/
+		//Compare an actual result vs expected result.
+		System.out.println("Validating title.");
+		Assert.assertEquals(actual_title, expected_title);
 		
 		/*//isElementPresent("xpath") - True, False
 		Assert.assertTrue(false, "Element not found.");*/
 		
-		//Forcefully fail a test case.
-		Assert.fail("Failing the test as the condition is not met.");
+		System.out.println("Validating image.");
+		Assert.assertEquals(true, false);
+		
+		System.out.println("Valiating Text box presence.");
+		Assert.assertEquals(true, false);
+		
+		/*//Forcefully fail a test case.
+		Assert.fail("Failing the test as the condition is not met.");*/
 		
 		System.out.println("Ending");
 	}
