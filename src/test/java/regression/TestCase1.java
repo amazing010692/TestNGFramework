287bcc164ba42f6c50718eb644031c18a9e4da3a
amazing010692 <hello_janielle1992@yahoo.com.ph>
2020-07-02 11:14:04 +0800

    1st commit - TestNG - Managing Multiple Test Suites

---
+++ src/test/java/regression/TestCase1.java
@@ -0,0 +1,41 @@
+package regression;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class TestCase1 {
+	
+	@BeforeTest
+	public void creatingDBConn() {
+		System.out.println("Creating DB connection");
+	}
+	
+	@AfterTest
+	public void closingDBConn() {
+		System.out.println("Closing DB connection");
+	}
+	
+	@BeforeMethod
+	public void launchBrowser() {
+		System.out.println("Launching the Browser");
+	}
+	
+	@AfterMethod
+	public void closeBrowser() {
+		System.out.println("Closing the Browser");
+	}
+	
+	@Test(priority = 1)
+	public void doUserReg() {
+		System.out.println("Executing User Registration test");
+	}
+	
+	@Test(priority = 2)
+	public void doLogin() {
+		System.out.println("Executing login test");
+	}
+
+}
---
+++ src/test/java/regression/TestCase2.java
@@ -0,0 +1,45 @@
+package regression;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+public class TestCase2 {
+	
+	@Test
+	public void validateTitles() {
+		System.out.println("Beginning");
+		
+		String expected_title = "Yahoo.com";	//excel
+		String actual_title = "Gmail.com";		//selenium
+		
+		/*if(expected_title.equals(actual_title)) {
+			System.out.println("Test Case Passed.");
+		} else {
+			System.out.println("Test Case Failed.");
+		}*/
+		
+		SoftAssert softAssert = new SoftAssert();
+		
+		//Compare an actual result vs expected result.
+		System.out.println("Validating title.");
+		softAssert.assertEquals(actual_title, expected_title);
+		
+		/*//isElementPresent("xpath") - True, False
+		Assert.assertTrue(false, "Element not found.");*/
+		
+		System.out.println("Validating image.");
+		softAssert.assertEquals(true, false, "Image not present.");
+		
+		System.out.println("Validating Text box presence.");
+		softAssert.assertEquals(true, false, "Text box not present.");
+		
+		/*//Forcefully fail a test case.
+		Assert.fail("Failing the test as the condition is not met.");*/
+		
+		System.out.println("Ending");
+		
+		softAssert.assertAll();
+	}
+
+}
