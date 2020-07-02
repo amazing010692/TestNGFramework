fe8d4483f9cba73b6a34e6b238b19d0e6f5cf83b
amazing010692 <hello_janielle1992@yahoo.com.ph>
2020-07-02 17:29:40 +0800

    3rd commit - TestNG - Test Groups

--- src/test/java/testcases/TestCase3.java
+++ src/test/java/testcases/TestCase3.java
@@ -5,23 +5,23 @@ import org.testng.annotations.Test;
 
 public class TestCase3 {
 	
-	@Test(priority = 1)
+	@Test(priority = 1, groups = {"functional", "smoke"})
 	public void doUserReg() {
 		System.out.println("Executing User Registration test");
 		Assert.fail("User is not registered successfully.");
 	}
 	
-	@Test(priority = 2, dependsOnMethods = "doUserReg")
+	@Test(priority = 2, dependsOnMethods = "doUserReg", groups = {"functional", "smoke"})
 	public void doLogin() {
 		System.out.println("Executing login test");
 	}
 	
-	@Test(priority = 3, dependsOnMethods = "doUserReg", alwaysRun = true)
+	@Test(priority = 3, dependsOnMethods = "doUserReg", alwaysRun = true, groups = {"functional", "smoke"})
 	public void thirdTest() {
 		System.out.println("Executing Third Test");
 	}
 	
-	@Test(priority = 4, dependsOnMethods = "doUserReg")
+	@Test(priority = 4, groups = "bvt")
 	public void fourthTest() {
 		System.out.println("Executing Fourth Test");
 	}
