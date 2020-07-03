c4c2f45ee1f3077a35c2cc8077494d7b69277894
amazing010692 <hello_janielle1992@yahoo.com.ph>
2020-07-03 20:48:08 +0800

    4th commit - TestNG - Adding Before and After Suite annotations

--- src/test/java/testcases/BaseTest.java
+++ src/test/java/testcases/BaseTest.java
@@ -5,5 +5,9 @@ public class BaseTest {
 	public void setUp() {
 		System.out.println("Initializing Everything !!!");
 	}
+	
+	public void tearDown() {
+		System.out.println("Quitting everything !!!");
+	}
 
 }
