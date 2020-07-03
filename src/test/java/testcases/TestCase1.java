fda656094658b786ee881456fa39ce5701fbc697
amazing010692 <hello_janielle1992@yahoo.com.ph>
2020-07-03 11:50:01 +0800

    5th commit - TestNG - TestNG Listeners

--- src/test/java/testcases/TestCase1.java
+++ src/test/java/testcases/TestCase1.java
@@ -7,35 +7,45 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class TestCase1 {
-	
+
 	@BeforeTest
 	public void creatingDBConn() {
 		System.out.println("Creating DB connection");
 	}
-	
+
 	@AfterTest
 	public void closingDBConn() {
 		System.out.println("Closing DB connection");
 	}
-	
+
 	@BeforeMethod
 	public void launchBrowser() {
 		System.out.println("Launching the Browser");
 	}
-	
+
 	@AfterMethod
 	public void closeBrowser() {
 		System.out.println("Closing the Browser");
 	}
-	
+
 	@Test(priority = 1, groups = "functional")
 	public void doUserReg() {
-		System.out.println("Executing User Registration test");
+
+		try {
+			System.out.println("Executing User Registration test");
+		} catch (Throwable t) {
+			System.out.println("Capturing Screenshot");
+		}
+
 	}
-	
+
 	@Test(priority = 2, groups = "functional")
 	public void doLogin() {
-		System.out.println("Executing login test");
+		try {
+			System.out.println("Executing login test");
+		} catch (Throwable t) {
+			System.out.println("Capturing Screenshot");
+		}
 	}
 
 }
