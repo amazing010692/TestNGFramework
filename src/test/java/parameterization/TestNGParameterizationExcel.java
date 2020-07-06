0791a82f4cb1bafb6a3777a4c151b0daf3463fe1
amazing010692 <hello_janielle1992@yahoo.com.ph>
2020-07-06 13:41:00 +0800

    3rd commit - TestNG Parameterization Excel Reading

--- src/test/java/parameterization/TestNGParameterizationExcel.java
+++ src/test/java/parameterization/TestNGParameterizationExcel.java
@@ -1,8 +1,14 @@
 package parameterization;
 
 import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 
 public class TestNGParameterizationExcel {
+	
+	@Test(dataProvider = "getData")
+	public void testData() {
+		
+	}
 	
 	@DataProvider
 	public static Object[][] getData() {
