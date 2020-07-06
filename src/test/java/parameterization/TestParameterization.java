a4eabc392951f474ac94e4c6215923b156ef255c
amazing010692 <hello_janielle1992@yahoo.com.ph>
2020-07-06 09:55:56 +0800

    2nd commit - Understanding Parameterization through DataProviders

--- src/test/java/parameterization/TestParameterization.java
+++ src/test/java/parameterization/TestParameterization.java
@@ -1,5 +1,12 @@
 package parameterization;
 
+import org.testng.annotations.Test;
+
 public class TestParameterization {
+	
+	@Test
+	public void doLogin(String username, String password) {
+		System.out.println(username + "---" + password);
+	}
 
 }
