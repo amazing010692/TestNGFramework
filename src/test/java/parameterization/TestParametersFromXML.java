d6582e118f523833de48efeadf117a83cdb97608
amazing010692 <hello_janielle1992@yahoo.com.ph>
2020-07-08 17:52:04 +0800

    4th commit - Reading Parameters from TestNG.xml file

--- src/test/java/parameterization/TestParametersFromXML.java
+++ src/test/java/parameterization/TestParametersFromXML.java
@@ -1,9 +1,14 @@
 package parameterization;
 
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
 public class TestParametersFromXML {
 	
+	@Parameters({"browser"})
+	@Test
 	public void getBrowser(String browser) {
-		
+		System.out.println(browser);
 	}
 
 }
