9bc50006013a5ed0e450cde465dec425082e3ebf
amazing010692 <hello_janielle1992@yahoo.com.ph>
2020-07-08 14:51:38 +0800

    10th commit - Handling Multiple Data Providers

--- src/test/java/parameterization/LoginTest.java
+++ src/test/java/parameterization/LoginTest.java
@@ -6,8 +6,14 @@ public class LoginTest {
 	
 	@Test(dataProviderClass = DataProviders.class, dataProvider = "dp1")
 	//Add arguments based on the columns.
-	public void testData(String username, String password) {
+	public void testLogin(String username, String password) {
 		System.out.println(username + "---" + password);
+	}
+	
+	@Test(dataProviderClass = DataProviders.class, dataProvider = "dp1")
+	//Add arguments based on the columns.
+	public void testUserReg(String fName, String lName, String email) {
+		System.out.println(fName + "---" + lName + "---" + email);
 	}
 
 }
