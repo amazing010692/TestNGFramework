c0ac8b9650f53248221f206bf1698f2d12f1f83e
amazing010692 <hello_janielle1992@yahoo.com.ph>
2020-07-08 13:58:22 +0800

    3rd commit - Handling Multiple Data Providers

--- src/test/java/parameterization/DataProviders.java
+++ src/test/java/parameterization/DataProviders.java
@@ -7,6 +7,10 @@ public class DataProviders {
 	@DataProvider(name = "dp1")
 	public static Object[][] getData() {
 		
+		//Create an object array for 2 rowns and 2 columns.
+		Object[][] data = new Object[2][2];
+		return data;
+		
 	}
 
 }
