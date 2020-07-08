05b97bbfa1f0e0041a399086a99909a4f1b312d3
amazing010692 <hello_janielle1992@yahoo.com.ph>
2020-07-08 14:21:41 +0800

    9th commit - Handling Multiple Data Providers

--- src/test/java/parameterization/LoginTest.java
+++ src/test/java/parameterization/LoginTest.java
@@ -4,7 +4,7 @@ import org.testng.annotations.Test;
 
 public class LoginTest {
 	
-	@Test(dataProvider = "dp1")
+	@Test(dataProviderClass = DataProviders.class, dataProvider = "dp1")
 	//Add arguments based on the columns.
 	public void testData(String username, String password) {
 		System.out.println(username + "---" + password);
