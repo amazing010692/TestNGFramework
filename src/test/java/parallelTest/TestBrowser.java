34b6a812685fc07cd6525d07437aa78ffe540738
amazing010692 <hello_janielle1992@yahoo.com.ph>
2020-07-09 09:58:24 +0800

    4th commit - Reading Parameters from TestNG.xml file

--- src/test/java/parallelTest/TestBrowser.java
+++ src/test/java/parallelTest/TestBrowser.java
@@ -1,5 +1,15 @@
 package parallelTest;
 
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
 public class TestBrowser {
+	
+	@Parameters({"browser"})
+	@Test
+	public void doLogin(String b) throws InterruptedException {
+		System.out.println("Browser name: " + b);
+		Thread.sleep(2000);
+	}
 
 }
