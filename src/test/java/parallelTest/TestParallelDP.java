b31dcf8c24f217795ac029246f9bd2d85818a887
amazing010692 <hello_janielle1992@yahoo.com.ph>
2020-07-09 10:52:42 +0800

    2nd commit - Running Parallel Test using DataProvider

--- src/test/java/parallelTest/TestParallelDP.java
+++ src/test/java/parallelTest/TestParallelDP.java
@@ -1,5 +1,17 @@
 package parallelTest;
 
+import java.util.Date;
+
+import org.testng.annotations.Test;
+
 public class TestParallelDP {
+	
+	@Test(dataProvider = "getData")
+	public void doLogin(String b) throws InterruptedException {
+		
+		Date d = new Date();
+		System.out.println("Browser name: " + b + "---" + d);
+		Thread.sleep(2000);
+	}
 
 }
