bb413fd5439ac382505a9f48ff35190bb5623eee
amazing010692 <hello_janielle1992@yahoo.com.ph>
2020-07-09 10:57:40 +0800

    3rd commit - Running Parallel Test using DataProvider

--- src/test/java/parallelTest/TestParallelDP.java
+++ src/test/java/parallelTest/TestParallelDP.java
@@ -2,6 +2,7 @@ package parallelTest;
 
 import java.util.Date;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class TestParallelDP {
@@ -12,6 +13,18 @@ public class TestParallelDP {
 		Date d = new Date();
 		System.out.println("Browser name: " + b + "---" + d);
 		Thread.sleep(2000);
+	}
+	
+	@DataProvider
+	public Object[][] getData() {
+		//2 rows and 1 column
+		Object[][] data = new Object[2][1];
+		
+		data[0][0] = "chrome";
+		data[1][0] = "firefox";
+		
+		return data;
+		
 	}
 
 }
