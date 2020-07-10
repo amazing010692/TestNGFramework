1f0ab9be70fd1741919aea311e2c6c6cf3d0b1fd
amazing010692 <hello_janielle1992@yahoo.com.ph>
2020-07-10 10:34:52 +0800

    2nd commit - InvocationCount and ThreadPoolSize

--- src/test/java/parallelTest/TestInvocation.java
+++ src/test/java/parallelTest/TestInvocation.java
@@ -1,5 +1,12 @@
 package parallelTest;
 
+import org.testng.annotations.Test;
+
 public class TestInvocation {
+	
+	@Test(invocationCount = 5)
+	public void executeTest() {
+		System.out.println("Launching Chrome browser");
+	}
 
 }
