8b8db534250a7dfb67c6d4abf29534fdd3d4e2e2
amazing010692 <hello_janielle1992@yahoo.com.ph>
2020-07-11 17:45:49 +0800

    6th commit - OnTestFailure - CaptureScreenshots

--- src/test/java/testNGListenerOnFailure/LoginTest.java
+++ src/test/java/testNGListenerOnFailure/LoginTest.java
@@ -1,5 +1,18 @@
 package testNGListenerOnFailure;
 
-public class LoginTest {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+public class LoginTest extends TestBase {
+	
+	@Test
+	public void doLogin() {
+		WebElement username = driver.findElement(By.id("identifierId"));
+		username.sendKeys("java@way2automation.com");
+		
+		WebElement nextButtonU = driver.findElement(By.xpath("//*[@id=\"identifierNext\"]/span/span"));
+		nextButtonU.click();
+	}
 
 }
