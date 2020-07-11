1c627cbc0093c2214290745093737d7c7e2170fa
amazing010692 <hello_janielle1992@yahoo.com.ph>
2020-07-12 00:25:38 +0800

    15th commit - OnTestFailure - CaptureScreenshots

--- src/test/java/testNGListenerOnFailure/TestBase.java
+++ src/test/java/testNGListenerOnFailure/TestBase.java
@@ -10,25 +10,20 @@ import org.testng.annotations.BeforeSuite;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class TestBase {
-	
+
 	public static WebDriver driver;
-	
+
 	@BeforeSuite
 	public void setUp() {
-		if(driver == null) {
-			WebDriverManager.chromedriver().setup();
-			WebDriver driver = new ChromeDriver();
-			//Pre-conditions | Maximize the browser and apply implicit waits.
-			driver.manage().window().maximize();
-			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-			
-			//Navigate to this site for sample checkbox.
-			driver.get("https://facebook.com");
-			System.out.println("TITLE: " + driver.getTitle() + " | URL: " + driver.getCurrentUrl());
-		}
-		
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.get("https://facebook.com");
+		System.out.println("TITLE: " + driver.getTitle() + " | URL: " + driver.getCurrentUrl());
+
 	}
-	
+
 	@AfterSuite
 	public void tearDown() {
 		driver.quit();
