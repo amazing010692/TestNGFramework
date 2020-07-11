63baf52d1e1019aef5f6373d70cf91eaddeaa9d9
amazing010692 <hello_janielle1992@yahoo.com.ph>
2020-07-11 17:31:29 +0800

    3rd commit - OnTestFailure - CaptureScreenshots

--- src/test/java/testNGListenerOnFailure/TestBase.java
+++ src/test/java/testNGListenerOnFailure/TestBase.java
@@ -1,8 +1,15 @@
 package testNGListenerOnFailure;
 
+import org.openqa.selenium.WebDriver;
+
 public class TestBase {
 	
+	public static WebDriver driver;
+	
 	public void setUp() {
+		if(driver == null) {
+			
+		}
 		
 	}
 	
