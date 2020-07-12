d2995bbbc3395460d0c79ba6f16de81e676e9ef1
amazing010692 <hello_janielle1992@yahoo.com.ph>
2020-07-12 17:43:53 +0800

    31st commit - OnTestFailure - CaptureScreenshots

--- src/test/java/testNGListenerOnFailure/LoginTest.java
+++ src/test/java/testNGListenerOnFailure/LoginTest.java
@@ -7,16 +7,16 @@ import org.testng.annotations.Test;
 
 public class LoginTest extends TestBase {
 	
-	@Test
+	@Test(priority = 1)
 	public void doLogin() throws InterruptedException {
-		//Input email address.
+		//Input email address. This will fail because of invalid xpath inputted.
 		Thread.sleep(3000);
 		WebElement fieldEmail = driver.findElement(By.xpath("//*[@id='email_invalid']"));
 		fieldEmail.sendKeys("test_email@gmail.com");
 		System.out.println("TC1 - Successfully inputted email address.");
 	}
 	
-	@Test
+	@Test(priority = 2)
 	public void composeEmail() {
 		Assert.fail("Error in composing email");
 	}
