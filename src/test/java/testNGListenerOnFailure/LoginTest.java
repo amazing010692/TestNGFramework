56388e56b437b1bc43c1d0d247c1222331aa112d
amazing010692 <hello_janielle1992@yahoo.com.ph>
2020-07-12 12:01:47 +0800

    19th commit - OnTestFailure - CaptureScreenshots

--- src/test/java/testNGListenerOnFailure/LoginTest.java
+++ src/test/java/testNGListenerOnFailure/LoginTest.java
@@ -7,8 +7,9 @@ import org.testng.annotations.Test;
 public class LoginTest extends TestBase {
 	
 	@Test
-	public void doLogin() {
+	public void doLogin() throws InterruptedException {
 		//Input email address.
+		Thread.sleep(3000);
 		WebElement fieldEmail = driver.findElement(By.xpath("//*[@id='email']"));
 		fieldEmail.sendKeys("test_email@gmail.com");
 		System.out.println("Successfully inputted email address.");
--- src/test/java/testNGListenerOnFailure/TestBase.java
+++ src/test/java/testNGListenerOnFailure/TestBase.java
@@ -4,6 +4,12 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.opera.OperaDriver;
+import org.openqa.selenium.opera.OperaOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 
@@ -11,12 +17,44 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class TestBase {
 
-	public static WebDriver driver;
+	public WebDriver driver;
+	
+	public WebDriver getDriver(String browser) {
+		
+		if(browser.equals("chrome")) {
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+			
+		} else if(browser.equals("firefox")) {
+			WebDriverManager.firefoxdriver().setup();
+			driver = new FirefoxDriver();
+			
+		} else if(browser.equals("ie")) {			
+			WebDriverManager.iedriver().setup();
+			driver = new InternetExplorerDriver();
+			
+		} else if(browser.equals("edge")) {			
+			WebDriverManager.edgedriver().setup();
+			driver = new EdgeDriver();
+		
+		} else if(browser.equals("opera")) {			
+			DesiredCapabilities capabilities = new DesiredCapabilities();
+			OperaOptions options = new OperaOptions();
+			options.setBinary("C:\\Users\\hello\\AppData\\Local\\Programs\\Opera\\64.0.3417.73\\opera.exe");
+			capabilities.setCapability(OperaOptions.CAPABILITY, options);
+			
+			WebDriverManager.operadriver().setup();
+			driver = new OperaDriver(options);
+		
+		}
+		
+		return driver;
+	
+	}
 
 	@BeforeSuite
 	public void setUp() {
-		WebDriverManager.chromedriver().setup();
-		WebDriver driver = new ChromeDriver();
+		driver = getDriver("chrome");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.get("https://facebook.com");
