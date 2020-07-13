a01ec3e1ff54cda2f4f70cd0945bba4285f0901d
amazing010692 <hello_janielle1992@yahoo.com.ph>
2020-07-13 10:19:07 +0800

    4th commit - Retry Listener to Re-Run Failed Test Cases

--- src/test/java/retry/TestLogin.java
+++ src/test/java/retry/TestLogin.java
@@ -1,5 +1,17 @@
 package retry;
 
+import org.testng.annotations.Test;
+
 public class TestLogin {
+	
+	@Test(priority = 1)
+	public void doLogin() {
+		
+	}
+	
+	@Test(priority = 2)
+	public void registerUsers() {
+		
+	}
 
 }
