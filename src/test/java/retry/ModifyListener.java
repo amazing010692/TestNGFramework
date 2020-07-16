059a03a1b598a49c4e4a4a927797f7288ebef12b
amazing010692 <hello_janielle1992@yahoo.com.ph>
2020-07-16 23:02:59 +0800

    Modify Listeners.

--- src/test/java/retry/ModifyListener.java
+++ src/test/java/retry/ModifyListener.java
@@ -1,10 +1,45 @@
 package retry;
 
+import java.util.Set;
+
 import org.testng.ITestContext;
 import org.testng.ITestListener;
+import org.testng.ITestNGMethod;
 import org.testng.ITestResult;
 
 public class ModifyListener implements ITestListener {
+
+	public void onFinish(ITestContext context) {
+		Set<ITestResult> failedTests = context.getFailedTests().getAllResults();
+		for (ITestResult temp : failedTests) {
+			ITestNGMethod method = temp.getMethod();
+			System.out.println("METHOD NAME : "+method.getMethodName());
+			if (context.getFailedTests().getResults(method).size() > 1) {
+				failedTests.remove(temp);
+				System.out.println("METHOD NAME : "+method.getMethodName());
+				
+			} else {
+				if (context.getPassedTests().getResults(method).size() > 0) {
+					failedTests.remove(temp);
+					
+				}
+			}
+			
+			Set<ITestResult> failedTests2 = context.getFailedTests().getAllResults();
+			System.out.println("Failed test size is: "+failedTests2.size());
+			
+			String value=null;
+			
+			for (ITestResult temp2 : failedTests2) {
+				ITestNGMethod method2 = temp2.getMethod();
+				value=method2.getMethodName();
+				System.out.println("METHOD NAME : "+value);
+				
+				
+			}
+		}
+		
+	}
 
 	public void onTestStart(ITestResult result) {
 		// TODO Auto-generated method stub
@@ -36,9 +71,5 @@ public class ModifyListener implements ITestListener {
 		
 	}
 
-	public void onFinish(ITestContext context) {
-		
-		
-	}
 
 }
